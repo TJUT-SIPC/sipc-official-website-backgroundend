7cc2c8538498e5a04e090b0a7ff43ab521e435b1
BA_NANA <798998087@qq.com>
2019-12-10 20:08:39 +0800

    数据库表

--- sql/table_dynamics.sql
+++ sql/table_dynamics.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `dynamics`(
-		`dynamic_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '动态编号',
-		`dynamic_image` VARCHAR(128) NOT NULL COMMENT '动态图片',
-		`dynamic_header` VARCHAR(50) NOT NULL COMMENT '动态标题',
-		`dynamic_text` text NOT NULL COMMENT '动态内容',
-		`dynamic_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '动态修改时间',
-		`dynamic_editor` VARCHAR(50) DEFAULT NULL COMMENT '发布者',
-		`dynamic_catagory` int(11) DEFAULT 1 COMMENT '分类（待定）',
-		PRIMARY KEY(`dynamic_id`)
-)ENGINE=INNODB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
+CREATE TABLE `dynamics` (
+  `dynamic_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '动态编号',
+  `dynamic_image` varchar(128) NOT NULL COMMENT '动态图片',
+  `dynamic_header` varchar(50) NOT NULL COMMENT '动态标题',
+  `dynamic_text` text NOT NULL COMMENT '动态内容',
+  `dynamic_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '动态修改时间',
+  `dynamic_editor` varchar(50) DEFAULT NULL COMMENT '发布者',
+  `dynamic_catagory` int(11) DEFAULT '1' COMMENT '分类（待定）',
+  PRIMARY KEY (`dynamic_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- sql/table_wishes.sql
+++ sql/table_wishes.sql
@@ -1,7 +1,8 @@
-CREATE TABLE `wishes`(
-		`wish_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '寄语编号',
-		`wish_name` VARCHAR(50) NOT NULL COMMENT '寄语发送者',
-		`wish_word` text NOT NULL COMMENT '寄语内容',
-		`wish_status` int(11) DEFAULT 1 COMMENT '0为审核不通过，1为待审核，2为审核通过待发布，3为已发布',
-		PRIMARY KEY(`wish_id`)
-)ENGINE=INNODB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
+CREATE TABLE `wishes` (
+  `wish_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '寄语编号',
+  `wish_name` varchar(50) NOT NULL COMMENT '寄语发送者',
+  `wish_word` text NOT NULL COMMENT '寄语内容',
+  `wish_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '寄语提交时间',
+  `wish_status` int(11) DEFAULT '1' COMMENT '0为审核不通过，1为待审核，2为审核通过待发布，3为已发布',
+  PRIMARY KEY (`wish_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
