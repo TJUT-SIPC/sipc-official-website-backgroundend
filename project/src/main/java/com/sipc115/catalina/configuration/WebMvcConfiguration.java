74ecf427ad60a55552de7ae6060c67e05ae22f8a
BA_NANA <798998087@qq.com>
2019-12-11 19:54:05 +0800

    dev/1.0.0

--- project/src/main/java/com/sipc115/catalina/configuration/WebMvcConfiguration.java
+++ project/src/main/java/com/sipc115/catalina/configuration/WebMvcConfiguration.java
@@ -8,8 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     public void addResourceHandlers(ResourceHandlerRegistry registry){
+
         registry.addResourceHandler("/sipc115_resources/**").addResourceLocations("file:/Volumes/disk3/天津理工大学/程序员之路/115官网项目/sipc115_resources/");
+
         super.addResourceHandlers(registry);
+
     }
 
 }
